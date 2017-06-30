395e72f5ff8897ea9d5534f1c042681c703b263d
SNAKE169 <ducanh6901@gmail.com>
2017-07-01 06:00:15 +0700

    a

--- index.js
+++ index.js
@@ -24,8 +24,13 @@ app.use('/q4', require('./q4'));
 app.use('/q5', require('./q5'));
 app.use('/q6', require('./q6'));
 app.use('/q7', require('./q7'));
-app.use('/q9', require('./q8'));
+app.use('/q8', require('./q8'));
 app.use('/q9', require('./q9'));
+app.use('/q10', require('./q10'));
+app.use('/q11', require('./q11'));
+app.use('/q12', require('./q12'));
+app.use('/q13', require('./q13'));
+app.use('/q14', require('./q14'));
 const template = fs.readFileSync('templates/index1.html', 'utf-8');
 
 
