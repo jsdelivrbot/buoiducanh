d9f93be980d718750c0698811b85652594461a77
SNAKE169 <ducanh6901@gmail.com>
2017-07-01 07:19:43 +0700

    as

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 
-// views is directory for all template files
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
@@ -15,8 +14,6 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/add', require('./add'));
-app.use('/rankings', require('./rankings'));
 app.use('/q1', require('./q1'));
 app.use('/q2', require('./q2'));
 app.use('/q3', require('./q3'));
